70c5750a2ab56877e492b8839e2626df4d4b799e
Ahemd <ahmedsaye23156@gmail.com>
2023-10-14 10:57:04 +0300

    python can use multiple equation

--- chapter1.py
+++ chapter1.py
@@ -1,18 +1,6 @@
-#  not operator
-# A= True
-# print(not A)
+# python provide muiltiple sums like ::
 
-
-# pi = 3
-# radius = 11
-# area = pi * (radius**2)
-# radius = 14
-
-
-def findArea(pi,radius):
-    print(pi * (radius**2))
-
-radius = input("write the raduis: ")
-Pi = input("whrite tehe value of Pi: ")
-
-findArea(int(Pi),int(radius))
+A= 10
+B = 5
+(A,B) = (B,A)
+print(A)
