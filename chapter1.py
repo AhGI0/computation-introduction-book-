0dd58a6776e407589d43b72e70c029dc5846b152
Ahemd <ahmedsaye23156@gmail.com>
2023-10-14 10:49:23 +0300

    new areas

--- chapter1.py
+++ chapter1.py
@@ -1,5 +1,18 @@
 #  not operator
+# A= True
+# print(not A)
 
-A= True
 
-print(not A)
+# pi = 3
+# radius = 11
+# area = pi * (radius**2)
+# radius = 14
+
+
+def findArea(pi,radius):
+    print(pi * (radius**2))
+
+radius = input("write the raduis: ")
+Pi = input("whrite tehe value of Pi: ")
+
+findArea(int(Pi),int(radius))
