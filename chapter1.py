ffc3994c87522d44d7daf0911dfd9a56c05be26a
Ahemd <ahmedsaye23156@gmail.com>
2023-10-15 09:43:41 +0300

    try to avoid using the infinite loop it sucks

--- chapter1.py
+++ chapter1.py
@@ -1,11 +1,9 @@
 # we have to find a way (finding the best way to cube a number!)
 
 #3
+num = int(input("Enter a positive number"))
+A= 0
+while num > 0:
+    A += 1
+    print(A)
 
-def cube ( num):
-    espsilon = .01
-    guess = num / .02
-    while  abs(guess **3 - num >= espsilon):
-        guess = guess- (guess**3 - num )/ (3* guess**2)
-    print(f'the guess num  of {num} is {round(guess)}')
-cube(1957816251)
