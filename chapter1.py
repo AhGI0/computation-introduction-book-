ba4ffc99876398d67239c1558001e3660d390b44
Ahemd <ahmedsaye23156@gmail.com>
2023-10-15 09:41:59 +0300

    here you used the round built-in function

--- chapter1.py
+++ chapter1.py
@@ -7,5 +7,5 @@ def cube ( num):
     guess = num / .02
     while  abs(guess **3 - num >= espsilon):
         guess = guess- (guess**3 - num )/ (3* guess**2)
-    print(f'the guess num  of {num} is {guess}')
+    print(f'the guess num  of {num} is {round(guess)}')
 cube(1957816251)
