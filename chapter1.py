27edecbb8769152688aed14daa3d76b88411507c
Ahemd <ahmedsaye23156@gmail.com>
2023-10-23 20:17:14 +0300

    awesome

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,15 @@
-ar = ["Ahmed", "sayed", "nada", "Maryam", "Esraa"]
+# i want to make an itergraion to know how many lines i have in my file
 
-print(ar[len(ar)-1])  # it will print out the last name in the last array
+A = open("file.txt", "rt")
+tracker = 0
+new = []
+
+for i in A:
+
+    tracker += 1
+    # print(f'{tracker} {i}')
+
+print(f'u have {tracker} lines')
+
+
+# print(tracker)s
